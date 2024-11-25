563a5265b10d0e4e71018955cf5eb6a082d68a2d
S18-Niloy <d19niloy@gmail.com>
2024-11-25 16:03:34 +0600

    Environment setup

---
+++ Controllers/Sales/OrderController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ERP.Controllers.Sales
+{
+    public class OrderController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Controllers/Shipping/ShipmentController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ERP.Controllers.Shipping
+{
+    public class ShipmentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
